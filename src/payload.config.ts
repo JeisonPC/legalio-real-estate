add729377ccd7c6984cd0b035808ad16de799754
Jeison Fabian Poveda Chavez <jeison.povedac@gmail.com>
2026-04-27 02:27:57 -0500

    ADD: Se agregan cambios de fix de correo

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -22,6 +22,10 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
+  serverURL:
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    process.env.PAYLOAD_PUBLIC_SERVER_URL ||
+    "http://localhost:3000",
   admin: {
     user: Users.slug,
     importMap: {
